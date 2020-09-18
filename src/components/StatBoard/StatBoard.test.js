1bae3ad14d1828c6b0f85b3354d49a472fec40e8
David Olubusoye <davidolubusoye@gmail.com>
2020-09-18 09:29:37 +0100

    update StatBoard test

--- src/components/StatBoard/StatBoard.test.js
+++ src/components/StatBoard/StatBoard.test.js
@@ -2,9 +2,16 @@ import React from "react";
 import { shallow } from "enzyme";
 
 import StatBoard from "./StatBoard";
+import StatCard from "../StatCard/StatCard";
 
 describe("<StatBoard />", () => {
 	it("renders a StatBoard component", () => {
 		expect(shallow(<StatBoard />)).toMatchSnapshot();
+	});
+
+	it("renders one or more StatCard component", () => {
+		const wrapper = shallow(<StatBoard />);
+
+		expect(wrapper.containsMatchingElement(StatCard)).toBeTruthy();
 	});
 });
