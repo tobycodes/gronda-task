659f463839ce8c3d867c1af906483de05f6af47f
David Olubusoye <davidolubusoye@gmail.com>
2020-09-18 09:40:46 +0100

    Updates

---
+++ precache-manifest.c859421348122f4db0c752617135526b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9361317ac9453e74d762a6a0b96fe59",
+    "url": "/gronda-task/index.html"
+  },
+  {
+    "revision": "9d6cb7b53c9e2fd1cdf4",
+    "url": "/gronda-task/static/css/main.ec5657e2.chunk.css"
+  },
+  {
+    "revision": "43917f5236663e872250",
+    "url": "/gronda-task/static/js/2.b93b45ef.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gronda-task/static/js/2.b93b45ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d6cb7b53c9e2fd1cdf4",
+    "url": "/gronda-task/static/js/main.ade01316.chunk.js"
+  },
+  {
+    "revision": "18adff186dca1de1d0a0",
+    "url": "/gronda-task/static/js/runtime-main.c797b19f.js"
+  }
+]);
